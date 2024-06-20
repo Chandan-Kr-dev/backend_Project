2fabfbcb075343d9bf265fa2c96e37f9bc12c24d
Chandan-kr-dev <chandanramgar@gmail.com>
2024-06-20 23:18:07 +0530

    Add utils files and discussed about it

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ dotenv.config({path:'./env'})
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.POT || 8000,()=>{
+        console.log(`Server is running at Port: ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+    console.log("MONGO DB connection Failed !!!",err)
+})
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,47 @@
+const asyncHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler} 
+
+
+
+
+
+
+
+// these are the higher order function where the function itself accepts a function in its parameter
+
+// const asyncHandler=()=>{}
+// const asyncHandler=()=>()=>{}
+// const asyncHandler=()=> async ()=>{}
+
+
+
+
+
+// first way
+
+    // const asyncHandler =(requestHandler)=>{
+    //     (req,res,next)=>{
+    //         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    //     }
+    // }
+    
+// second way
+// this can be done using try catch 
+
+// const asyncHandler =(fn)=>async(req,res,next)=> {
+//     try {
+//         await fn(req,res,next);
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message,
+//         })
+//     }
+// }
